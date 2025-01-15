da006b1346496ab2a656d13d46f48978ba330e2d
ShionNakamura <shionnakamura0120@gmail.com>
2025-01-15 14:08:02 +0900

    Initial Commit

---
+++ PFCTracker/PFCTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PFCTrackerApp.swift
+//  PFCTracker
+//
+//  Created by 仲村士苑 on 2025/01/15.
+//
+
+import SwiftUI
+
+@main
+struct PFCTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PFCTrackerTests/PFCTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PFCTrackerTests.swift
+//  PFCTrackerTests
+//
+//  Created by 仲村士苑 on 2025/01/15.
+//
+
+import Testing
+@testable import PFCTracker
+
+struct PFCTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
